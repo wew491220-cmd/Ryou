6522733c21a561586a7b99514153b473bf22ea51
wew491220-cmd <wew491220@gmail.com>
2026-09-16 21:50:42 +0700

    Update pr8.sh

--- pr8.sh
+++ pr8.sh
@@ -46,7 +46,7 @@ class ServerController extends ClientApiController
         $authUser = Auth::user();
 
         if ($authUser->id !== 1 && (int) $server->owner_id !== (int) $authUser->id) {
-            abort(403, '🚫 ANTI INTIP - KEAMANAN BY @REXZYSTR');
+            abort(403, '🚫 ANTI INTIP - KEAMANAN BY @RYOUKEN00');
         }
 
         return $this->fractal->item($server)
